b4def6ea90699c0114c796261d44e97b26d6a82b
Ryan Cool <ryantcool@pm.me>
2024-06-14 14:12:13 -0400

    cleaned up completion code

--- config-vpn.bash
+++ config-vpn.bash
@@ -4,7 +4,7 @@
 
 _config-vpn_completion()
 	{
-		COMPREPLY=($(compgen -W "connect disconnect" "${COMP_WORDS[1]}"))
+		readarray -t COMPREPLY < <(compgen -W "connect disconnect" "${COMP_WORDS[1]}")
 	}
 
 complete -F _config-vpn_completion config-vpn
