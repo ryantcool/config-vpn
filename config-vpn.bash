2ebf0aee3d94e8cdae493c065d69fdee869d61f3
Ryan Cool <ryantcool@pm.me>
2025-02-14 16:29:40 -0500

    switched first COMPREPLY to mapfile to make shllcheck happy

--- config-vpn.bash
+++ config-vpn.bash
@@ -8,20 +8,17 @@ _config_vpn_completion() {
 
     # Sets completion for main options (connect, disconnect)
     if [[ ${COMP_CWORD} -eq 1 ]]; then
-        # If it's the first potential option, then
-	# complete with connect or disconnect
-        COMPREPLY=($(compgen -W "$opts" -- "$cur"))
+    # If it's the first potential option, then complete with connect or disconnect
+        mapfile -t COMPREPLY < <(compgen -W "$opts" -- "$cur")
         return 0
     fi
 
     # Set completion options if help flag is passed
     if [[ $prev == "-h" || $prev == "--help" ]]; then
-        # return empty array, completion not needed after
-	# -h or --help is typed
+    # return empty array, completion not needed after -h or --help is typed
         COMPREPLY=()
         return 0
     fi
 }
 
 complete -F _config_vpn_completion config-vpn
-
